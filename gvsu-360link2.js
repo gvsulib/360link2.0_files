40275da06bd19df784fce46322b4e356e71f1abc
Matthew Reidsma <reidsmam@gvsu.edu>
2015-04-21 13:19:44 -0400

    Added line to style psuedo header in ProQuest code

--- gvsu-360link2.js
+++ gvsu-360link2.js
@@ -11,7 +11,7 @@ setTimeout(function() {
 		jQuery('.custom-links').find('div:only-of-type').append('<div class="gvsu-custom-link"><a href="mailto:erms@gvsu.edu?subject=Problem%20Linking%20to%20Full%20Text&amp;body=' + encodeURIComponent(location.pathname) + encodeURIComponent(location.search) + '">Found a problem? Let our crack team of link-fixers know!</a>');
 		
 		// Attempt to style the elementless heading that divides browse and full text
-		jQuery('div.resource-row:contains["Browse the online journal by issue or volume"]').css('font-weight', 'bold').next('.resource-description-text').css('font-weight','normal');
+		jQuery('div.resource-row:contains("Browse the online journal by issue or volume")').css('font-weight', 'bold').css('font-size', '1.2em').find('a').css('font-size','.9em').next('.resource-description-text').css('font-weight','normal').css('font-size','.8em');
 	
 		// Fix the poorly worded 'not available' link
 		if(jQuery('body').length > 0) {
